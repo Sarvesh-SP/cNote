023c73365f4f0e18319e105d1a8d50ddb463be7d
Sarvesh-SP <sarvesh.sp2013@gmail.com>
2021-05-28 14:43:19 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const chalk = require('chalk');
+const yargs = require('yargs');
+const notes = require('./utils.js');
+
+yargs.version('1.1.0')
+
+yargs.command( {
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: "Body of the note",
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: (argv) => {
+    console.log('Title: ', argv.title, '\nBody: ', argv.body)
+  }
+})
+
+yargs.command( {
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: () => {
+    console.log('Removing the note...')
+  }
+})
+
+
+yargs.command( {
+  command: 'list',
+  describe: 'listing out all  note',
+  handler: () => {
+    console.log('listing the note...')
+  }
+})
+
+yargs.command( {
+  command: 'read',
+  describe: 'Read a note',
+  handler: () => {
+    console.log('Reading the note...')
+  }
+})
+
+
+yargs.parse()
